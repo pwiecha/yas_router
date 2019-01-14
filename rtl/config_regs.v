90c8ae05854923f164de05c935b68b24c926bdab
Pawel Wiecha <pawel.wiecha@tdk-electronics.tdk.com>
2019-01-14 05:04:47 -0800

    Initial specification, gitignore and first verilog module

---
+++ rtl/config_regs.v
@@ -0,0 +1,19 @@
+// -------------------------------------------------------------
+// Module: config_regs.v
+// Author: PWI
+// Release: 1.0, Jan 2019
+// -------------------------------------------------------------
+
+module config_regs (
+  input [1:0] config_addr,
+  input [1:0] config_data,
+  input       config_en,
+  output      ch0_addr,
+  output      ch1_addr,
+  output      ch2_addr,
+  output      crc_en
+);
+
+
+
+endmodule
