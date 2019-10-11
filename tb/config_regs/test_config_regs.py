d5460b397b053bfbfa2828b03eb1b765053cb050
p wiecha <pawel.wiecha@gmail.com>
2019-10-11 05:08:14 -0700

    replaced cocotb modules with pip-installed, updated licence and gitignore

--- tb/config_regs/test_config_regs.py
+++ tb/config_regs/test_config_regs.py
@@ -1,33 +1,5 @@
-'''
-BSD 2-Clause License
-
-Copyright (c) 2019, TDK Electronics
-All rights reserved.
-
-Author: Pawel Wiecha, https://github.com/pwiecha
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
-#------------ Config regs submodule directed and constrained random tests --------------------------
+#---------- Config regs submodule containing
+#      directed and constrained random tests ----------
 import cocotb
 from cocotb.clock import Clock, Timer
 from cocotb.result import TestFailure, TestSuccess
@@ -107,3 +79,4 @@ def randomized_test(dut):
         raise TestSuccess(f"\nSummary: coverage achieved: {coverage:.2f}%")
     else:
         raise TestFailure(f"There were {error_cnt} readback failures")
+
