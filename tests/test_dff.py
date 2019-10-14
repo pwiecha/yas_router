53630285d381011d1bbedb466d2920a3ac66b834
p wiecha <pawel.wiecha@gmail.com>
2019-10-14 07:53:22 -0700

    added testcase for empty run()

--- tests/test_dff.py
+++ tests/test_dff.py
@@ -9,5 +9,13 @@ os.environ["SIM"] = 'questa'
 def test_dff_verilog():
     run(verilog_sources=["dff.v"], toplevel="dff_test", module="dff_cocotb")  # sources  # top level HDL  # name of cocotb test module
 
+os.environ["VERILOG_SOURCES"] = "dff.v"
+os.environ["MODULE"] = "dff_cocotb"
+os.environ["TOPLEVEL"] = "dff_test"
+os.environ["TOPLEVEL_LANG"] = "verilog"
+def test_dff_verilog2():
+    run()
+
 if __name__ == "__main__":
     test_dff_verilog()
+    test_dff_verilog2()
