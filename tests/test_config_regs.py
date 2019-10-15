e98bb3ef272bef31f1dae72967ff1b8de5818b93
p wiecha <pawel.wiecha@gmail.com>
2019-10-15 07:15:02 -0700

    first cocotb-test test working under questa and icarus

--- tests/test_config_regs.py
+++ tests/test_config_regs.py
@@ -14,15 +14,13 @@ os.environ["SIM"] = "icarus"
 #simulator = "questa"
 #os.environ["SIM"] = "questa" # has to be set from environ
 
-# cannot open file for reading problem
+compile_args = []
 if simulator == "icarus":
-    extra_args = [f"-c {cwd}/iverilog_precision"]
-else:
-    extra_args = []
+    compile_args = ["-c", f"{cwd}/iverilog_precision"]
 
-rtl_path = cwd + "/../rtl"
+rtl_path = cwd + "/../rtl/"
 rtl_files = ["config_regs.v"]
-rtl_list = [rtl_path+"/"+f for f in rtl_files]
+rtl_list = [rtl_path+f for f in rtl_files]
 
 def test_basic():
     run(
@@ -30,7 +28,7 @@ def test_basic():
         verilog_sources=rtl_list, # always a list
         toplevel="config_regs",
         module="tb_config_regs",
-        extra_args=extra_args)
+        compile_args=compile_args)
 
 
 if __name__ == "__main__":
