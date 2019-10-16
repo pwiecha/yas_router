bf97c05ff50c7993bb6d6e45118fe1f8505e8e0e
p wiecha <pawel.wiecha@gmail.com>
2019-10-16 07:03:30 -0700

    cocotb test script refactor + changed tb name

--- tests/test_config_regs.py
+++ tests/test_config_regs.py
@@ -3,33 +3,34 @@ from cocotb_test.run import run
 import sys, os
 import pytest
 
-cwd = os.getcwd()
-tb_path = cwd + "/../tb"
-sys.path.append(cwd)
-sys.path.append(tb_path)
+# Aliases
+updir = os.path.dirname
+join = os.path.join
 
-simulator = "icarus"
+# Change this to change simulator or set SIM variable in run shell
 os.environ["SIM"] = "icarus"
 
-#simulator = "questa"
-#os.environ["SIM"] = "questa" # has to be set from environ
+# Pathing
+cwd = os.getcwd()
+tbd = join(updir(cwd), "tb/")
+rtld = join(updir(cwd), "rtl/")
+rtl_files = [join(rtld, "config_regs.v")]
 
+# Simulator specific
 compile_args = []
+simulator = os.getenv("SIM", "icarus") # defaults to icarus when none set
 if simulator == "icarus":
-    compile_args = ["-c", f"{cwd}/iverilog_precision"]
+    icarus_cmd_file = join(cwd, "iverilog_precision")
+    compile_args = [f"-c{icarus_cmd_file}"]
+elif simulator == "questa":
+    compile_args = ["-timescale", "1ns/1ns"]
 
-rtl_path = cwd + "/../rtl/"
-rtl_files = ["config_regs.v"]
-rtl_list = [rtl_path+f for f in rtl_files]
-
+# Tests to run
 def test_basic():
     run(
-        #simulator=cocotb_test.simulator.Icarus, # cannot pass a string, only sim obj
-        verilog_sources=rtl_list, # always a list
+        verilog_sources=rtl_files, # always a list
         toplevel="config_regs",
         module="tb_config_regs",
-        compile_args=compile_args)
+        compile_args=compile_args,
+        python_search=[tbd])
 
-
-if __name__ == "__main__":
-    test_basic()
